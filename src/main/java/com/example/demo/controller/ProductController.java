9056834cc5be34ee12a1e2033493414c1bc8ab14
shivamK98 <shivam.kumar@connectandheal.com>
2022-05-23 18:29:00 +0530

    Get individual and put implemented in ProductController

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -2,12 +2,15 @@ package com.example.demo.controller;
 
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -43,7 +46,48 @@ public class ProductController {
 		}
 	}
 	
+	@GetMapping("/product/{id}")
+	public ResponseEntity<?> getSingleProduct(@PathVariable("id") String id){
+		Optional<productDTO> productOptional = productRepo.findById(id);
+		if(productOptional.isPresent()) {
+			return new ResponseEntity<>(productOptional.get(), HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>("Product not found with id " + id, HttpStatus.NOT_FOUND);					
+		}
+	}
+	
+	@PutMapping("/product/{id}")
+	public ResponseEntity<?> putById(@PathVariable("id") String id, @RequestBody productDTO productNew){
+		Optional<productDTO> productOptional = productRepo.findById(id);
+		if(productOptional.isPresent()) {
+			productDTO productSave = productOptional.get();
+			productSave.setProductName(productNew.getProductName() == null ? productSave.getProductName() : productNew.getProductName());
+			productSave.setPrice(productNew.getPrice() == 0 ? productSave.getPrice() : productNew.getPrice());
+			productSave.setUpdatedAt(productNew.getUpdatedAt() == null ? productSave.getUpdatedAt() : productNew.getUpdatedAt());
+			productRepo.save(productSave);
+			return new ResponseEntity<>(productSave, HttpStatus.OK);
+		}else {
+			return new ResponseEntity<>("Product not found with id " + id, HttpStatus.NOT_FOUND);
+		}
+	}
+	
+	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
